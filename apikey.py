28ddffd0e9d02918679731811453719fb876edfb
Raju Halder <140959078+Raj2366@users.noreply.github.com>
2024-08-31 00:26:16 +0530

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -9,7 +9,7 @@ import os
 
 
 
-engine = pyttsx3.init("sapi5")
+engine = pyttsx3.init(driverName='espeak')
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[1].id)
 engine.setProperty("rate",170)
