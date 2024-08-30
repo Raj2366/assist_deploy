fd3c9df729e1d9fdcea502e7e931e72943af7038
Raju Halder <140959078+Raj2366@users.noreply.github.com>
2024-08-31 00:26:54 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -8,7 +8,7 @@ from deep_translator import GoogleTranslator
 from gtts import gTTS 
 import os
 
-engine = pyttsx3.init("sapi5")
+engine = pyttsx3.init(driverName='espeak')
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[1].id)
 engine.setProperty("rate",170)
